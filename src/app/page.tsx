9558111b24774bfadabaa730835d8d4de7ab2b30
ms0223900 <ms0223900@gmail.com>
2023-11-24 22:20:28 +0800

    fix: fix and update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
 
-export async function getData() {
+async function getData() {
   const postListData = [1, 2, 3].map((id) => ({
     id: String(id),
   }));
+
   return {
     posts: postListData,
   };
@@ -17,14 +17,13 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         {homepageData.posts.map((post) => (
-          <div
+          <Link
             className="rounded-sm p-8 bg-white shadow-sm m-2 cursor-pointer hover:shadow-md"
             key={post.id}
+            href={`/post/${post.id}`}
           >
-            <Link href={`/post/${post.id}`}>
-              <h1>{`Post -- ${post.id}`}</h1>
-            </Link>
-          </div>
+            <h1>{`Post -- ${post.id}`}</h1>
+          </Link>
         ))}
       </div>
     </main>
