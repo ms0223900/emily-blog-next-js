31b162a650d12f2938b9c57f01bb32e10cac6ebe
ms0223900 <ms0223900@gmail.com>
2023-12-07 22:50:44 +0800

    fix: fix dynamic export

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,16 +1,16 @@
 import { NextPageContext } from "next";
 import { NextRequest } from "next/server";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
-export async function GET(
-  request: NextRequest,
-  context: NextPageContext
-): Promise<Response> {
-  console.log("url: ", request.url);
-  console.log("ctx: ", context);
+// export async function GET(
+//   request: NextRequest,
+//   context: NextPageContext
+// ): Promise<Response> {
+//   console.log("url: ", request.url);
+//   console.log("ctx: ", context);
 
-  return Response.json({
-    data: [],
-  });
-}
+//   return Response.json({
+//     data: [],
+//   });
+// }
