8ad66f6f098c201f4f029d9697e8b76d97b0577c
ms0223900 <ms0223900@gmail.com>
2023-12-11 21:48:31 +0800

    feat: update sheet api

--- src/mapper/config.ts
+++ src/mapper/config.ts
@@ -1,4 +1,4 @@
 export const SHEET_ID = process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID;
 export const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
 export const getSheetApiUrl = (sheetName: string) =>
-  `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${sheetName}!A1:K500?key=${API_KEY}`;
+  `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${sheetName}!A2:K500?key=${API_KEY}`;
