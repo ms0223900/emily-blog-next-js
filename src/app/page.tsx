9c59124ed1975c3da57e5c451d9769daf3d6f473
penguin-cho <penguin-cho@cloud-latitude.com>
2024-03-18 21:42:28 +0800

    add Banner.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,42 @@
 import HomeHomepageRepository from "@/repos/homepage/HomepageRepository";
 import Link from "next/link";
+import Banner from "@/components/homepage/Banner";
 
 async function getData() {
-  const homepageData = await HomeHomepageRepository.getData();
+    const homepageData = await HomeHomepageRepository.getData();
 
-  return homepageData;
+    return homepageData;
 }
 
 export default async function Home() {
-  const homepageData = await getData();
+    const homepageData = await getData();
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2 md:p-8 lg:p-24">
-      <div className="grid grid-cols-12 gap-2 flex-wrap">
-        {homepageData.posts.map((post) => (
-          <Link
-            className="block rounded-md p-2 bg-white shadow-sm cursor-pointer hover:shadow-md col-span-full md:col-span-6 grid-cols-6"
-            key={post.id}
-            href={`/post/${post.id}`}
-          >
-            <div className="grid grid-cols-2 gap-2">
-              <div className="col-span-1">
-                <img alt="thumbnail" src={post.thumbnail} />
-              </div>
-              <div className="col-span-1">
-                <h2>{post.title}</h2>
-                <hr />
-                <p>{post.description}</p>
-                <span className={"text-gray-400"}>{post.createTime}</span>
-              </div>
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-2 md:p-8 lg:p-24">
+            <Banner title={"【荷蘭阿姆斯特丹】鬱金香公園-根本是天堂8太美了"}
+                    intro={"兩間飯店都各有特色，其中第一間Aziza Paradise飯店比較靠近市區交通方便。而另一間Costa Palawan是靠海邊的度假村，景色超美非常適合好好的度假放鬆！"}/>
+
+            <div className="grid grid-cols-12 gap-2 flex-wrap">
+                {homepageData.posts.map((post) => (
+                    <Link
+                        className="block rounded-md p-2 bg-white shadow-sm cursor-pointer hover:shadow-md col-span-full md:col-span-6 grid-cols-6"
+                        key={post.id}
+                        href={`/post/${post.id}`}
+                    >
+                        <div className="grid grid-cols-2 gap-2">
+                            <div className="col-span-1">
+                                <img alt="thumbnail" src={post.thumbnail}/>
+                            </div>
+                            <div className="col-span-1">
+                                <h2>{post.title}</h2>
+                                <hr/>
+                                <p>{post.description}</p>
+                                <span className={"text-gray-400"}>{post.createTime}</span>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
             </div>
-          </Link>
-        ))}
-      </div>
-    </main>
-  );
+        </main>
+    );
 }
---
+++ src/components/homepage/Banner.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import React from 'react';
+import Link from "next/link";
+
+export interface BannerProps {
+    title: string;
+    intro: string;
+
+}
+
+function Tag({tag}: { tag: string }) {
+    return (
+        <Link href={`/tag/${tag}`}>
+            {tag}
+        </Link>
+    );
+}
+
+const Banner: React.FC<BannerProps> = (props) => {
+    return (
+        <div className={"bg-amber-400 w-[1200px] h-[600px] relative rounded-lg overflow-hidden"}>
+            <img className={"w-100 block h-auto"} alt={"banner-bg"}
+                 src={"https://images.unsplash.com/photo-1524386416438-98b9b2d4b433?q=65&w=1200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+            <div className={"max-w-[633px] p-10 pb-8 bg-white text-black rounded-2xl absolute left-16 bottom-4"}>
+                <Tag tag={"Travel"}/>
+                <h2 className={"text-3xl pb-4 font-bold"}>
+                    {props.title}
+                </h2>
+                <p className={"text-lg font-light"}>
+                    {props.intro}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
