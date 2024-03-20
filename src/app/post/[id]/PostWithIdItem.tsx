a1840504a8f75f48527d5e1dd20e620357c17dc3
penguin-cho <penguin-cho@cloud-latitude.com>
2024-03-20 22:22:14 +0800

    add react markdown

--- src/app/post/[id]/PostWithIdItem.tsx
+++ src/app/post/[id]/PostWithIdItem.tsx
@@ -1,13 +1,17 @@
 import { Post } from "@/repos/post/types";
 import React from "react";
+import Markdown from "react-markdown";
+import styles from './post.module.css'
 
 export const PostWithIdItem: React.FC<{
-  post: Pick<Post, "id" | "title" | "description" | "content">;
+    post: Pick<Post, "id" | "title" | "description" | "content">;
 }> = ({ post: { id, title, description, content } }) => (
-  <div>
-    <h1 className="text-2xl">{`Id: ${id} -- ${title}`}</h1>
-    <h2 className="text-xl">{description}</h2>
-    <hr />
-    <p className="whitespace-pre-wrap">{content}</p>
-  </div>
+    <div>
+        <h1 className="text-2xl">{`Id: ${id} -- ${title}`}</h1>
+        <h2 className="text-xl">{description}</h2>
+        <hr />
+        <Markdown className={styles.markdown}>
+            {content}
+        </Markdown>
+    </div>
 );
--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,21 +1,38 @@
 import PostRepository from "@/repos/post/PostRepository";
 import React, { memo } from "react";
 import { PostWithIdItem } from "./PostWithIdItem";
+import Head from "next/head";
+
+export const generateMetadata = async ({
+                                           params: { id }
+                                       }: { params: { id: string } }) => {
+    const postData = await PostRepository.getPostById(id);
+
+    return ({
+        title: postData.title,
+    })
+};
+
 
 const PostWithId: React.FC<{ params: { id: string } }> = async ({
-  params: { id },
-}) => {
-  const postData = await PostRepository.getPostById(id);
+                                                                    params: { id },
+                                                                }) => {
+    const postData = await PostRepository.getPostById(id);
 
-  return <PostWithIdItem post={postData} />;
+    return <div>
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
+        <PostWithIdItem post={postData} />
+    </div>
 };
 
 export async function generateStaticParams(): Promise<{ id: string }[]> {
-  const posts = await PostRepository.getPosts();
+    const posts = await PostRepository.getPosts();
 
-  return posts.map(({ id }) => ({
-    id: String(id),
-  }));
+    return posts.map(({ id }) => ({
+        id: String(id),
+    }));
 }
 
 export default memo(PostWithId);
