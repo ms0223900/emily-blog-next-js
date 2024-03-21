3ded0186d1c5100f49dc0e59b60826ae09229a9d
penguin-cho <penguin-cho@cloud-latitude.com>
2024-03-21 20:52:25 +0800

    update style

--- src/app/post/[id]/PostWithIdItem.tsx
+++ src/app/post/[id]/PostWithIdItem.tsx
@@ -6,12 +6,14 @@ import styles from './post.module.css'
 export const PostWithIdItem: React.FC<{
     post: Pick<Post, "id" | "title" | "description" | "content">;
 }> = ({ post: { id, title, description, content } }) => (
-    <div>
+    <div className={"max-w-[800px] w-full m-auto"}>
         <h1 className="text-2xl">{`Id: ${id} -- ${title}`}</h1>
         <h2 className="text-xl">{description}</h2>
         <hr />
-        <Markdown className={styles.markdown}>
-            {content}
-        </Markdown>
+        <div>
+            <Markdown className={styles.markdown}>
+                {content}
+            </Markdown>
+        </div>
     </div>
 );
