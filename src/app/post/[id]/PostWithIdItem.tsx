dd077f8790f24eb65180c443b733dcef59be8f9f
penguin-cho <penguin-cho@cloud-latitude.com>
2024-03-23 22:11:30 +0800

    remove unused useRef

--- src/app/post/[id]/PostWithIdItem.tsx
+++ src/app/post/[id]/PostWithIdItem.tsx
@@ -1,5 +1,5 @@
 import { Post } from "@/repos/post/types";
-import React, { useRef } from "react";
+import React from "react";
 import Markdown from "react-markdown";
 import styles from './post.module.css'
 import { Tag } from "@/components/homepage/Tag";
@@ -10,8 +10,6 @@ import remarkHeadingId from 'remark-heading-id';
 export const PostWithIdItem: React.FC<{
     post: Pick<Post, "id" | "title" | "description" | "content" | "tags">;
 }> = ({ post: { id, title, description, content, tags } }) => {
-    const markdownWrapperElRef = useRef<HTMLDivElement>(null);
-
     return (
         <div className={"max-w-[800px] w-full m-auto px-4 md:px-0 pt-8"}>
             <Tag tag={tags} />
@@ -21,16 +19,15 @@ export const PostWithIdItem: React.FC<{
                 <hr />
             </div>
             <div>
-                <div ref={markdownWrapperElRef}>
-                    <Markdown className={styles.markdown}
-                              remarkPlugins={[remarkGfm, remarkHeadingId]}
-                              components={{
-                                  a: ({ children, ...props }) =>
-                                      <a {...props} target={'_blank'}>{children}</a>
-                              }}
-                    >
-                        {content}
-                    </Markdown></div>
+                <Markdown className={styles.markdown}
+                          remarkPlugins={[remarkGfm, remarkHeadingId]}
+                          components={{
+                              a: ({ children, ...props }) =>
+                                  <a {...props} target={'_blank'}>{children}</a>
+                          }}
+                >
+                    {content}
+                </Markdown>
             </div>
         </div>
     );
