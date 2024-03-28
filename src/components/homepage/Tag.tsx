755f2a7f408305b61b35e6c0ddf56d6a8cf484cd
penguin-cho <penguin-cho@cloud-latitude.com>
2024-03-28 20:18:30 +0800

    temp tag direct to home

--- src/components/homepage/Tag.tsx
+++ src/components/homepage/Tag.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import React from "react";
 
-export function Tag({tag}: { tag: string }) {
+export function Tag({ tag }: { tag: string }) {
+    // const href = `/tag/${tag}`;
+    //TODO
+    const href = '/'
     return (
-        <Link className={'text-blue-300 p-2 bg-blue-50 rounded inline-block'} href={`/tag/${tag}`}>
+        <Link className={'text-blue-300 p-2 bg-blue-50 rounded inline-block'} href={href}>
             {tag}
         </Link>
     );
