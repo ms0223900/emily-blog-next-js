5e02e661789d29d36b14afe911704713ecfd2ccc
penguin-cho <penguin-cho@cloud-latitude.com>
2024-06-13 21:11:09 +0800

    feat: about me circle style

--- src/app/AboutMe.tsx
+++ src/app/AboutMe.tsx
@@ -1,6 +1,6 @@
 export function AboutMe() {
     return (
-        <div>
+        <div className={"flex flex-col gap-6"}>
             <div className={"flex gap-2 items-center"}>
                 <div className={'w-[38px] h-[32px] bg-primary'} />
                 <h2 className={"text-xl font-bold"}>
@@ -8,17 +8,17 @@ export function AboutMe() {
                 </h2>
             </div>
             <div className={'flex flex-col items-center m-auto w-60'}>
-                <div className={'bg-amber-100 rounded-full w-full pt-[100%]'}>
+                <div className={'bg-amber-100 rounded-full w-[186px] h-[186px]'}>
                     <img src={''} />
                 </div>
                 <h3 className={'py-4 font-bold text-xl'}>
                     自然捲
                 </h3>
-                <p>
-                    捲捲毛の部落格｜
-                    捲捲毛の部落格｜
-                    捲捲毛の部落格｜
-                    捲捲毛の部落格｜
+                <p className={"whitespace-pre-line text-center text-xs text-[#666666] leading-5"}>
+                    {`在有限的生命，努力活出最精采的自己
+期望將所學的經驗回饋社會
+期許透過文字與圖片
+為你帶來力量、感動與知識`}
                 </p>
             </div>
         </div>
