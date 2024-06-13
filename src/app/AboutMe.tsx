5328b547a44d9f5eb9656c223426a7d6a12a6b46
penguin-cho <penguin-cho@cloud-latitude.com>
2024-06-13 20:37:50 +0800

    feat: about me title

--- src/app/AboutMe.tsx
+++ src/app/AboutMe.tsx
@@ -1,9 +1,12 @@
 export function AboutMe() {
     return (
         <div>
-            <h2>
-                關於自然捲
-            </h2>
+            <div className={"flex gap-2 items-center"}>
+                <div className={'w-[38px] h-[32px] bg-primary'} />
+                <h2 className={"text-xl font-bold"}>
+                    關於自然捲
+                </h2>
+            </div>
             <div className={'flex flex-col items-center m-auto w-60'}>
                 <div className={'bg-amber-100 rounded-full w-full pt-[100%]'}>
                     <img src={''} />
